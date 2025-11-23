2a19ee9b9299fb564abce513b9a0bd4810b05cb6
alissxalv <alvaradoalissondalet@gmail.com>
2025-11-23 19:00:33 -0400

    Rango de edades

--- routes/RangoEdadRoutes.js
+++ routes/RangoEdadRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/RangoEdadController");
+const controller = require("../controllers/RangoEdadControlador");
 
 router.get("/", controller.list);
 router.get("/:id", controller.detail);
